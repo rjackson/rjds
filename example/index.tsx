50be493ec36dc625de30df6fbc69e44453aaebf0
Rob Jackson <rob@rjackson.me>
2023-03-31 16:40:51 +0100

    Add example app

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,73 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import * as ReactDOMClient from 'react-dom/client';
+
+import {
+  Anchor,
+  Button,
+  DescriptionList,
+  DescriptionListItem,
+  H2,
+  H3,
+  H4,
+  H5,
+  Header,
+  Input,
+  Panel,
+  Section,
+  SingleColumnLayout,
+  UnorderedList,
+} from '../.';
 
 const App = () => {
   return (
-    <div>
-      <Thing />
-    </div>
+    <SingleColumnLayout
+      header={
+        <Header className="text-center">
+          <h1>Single column layout header</h1>
+        </Header>
+      }
+      footer={
+        <footer>
+          <p>I am a footer</p>
+        </footer>
+      }
+    >
+      <Section>
+        <Panel className="flex flex-col space-y-4">
+          <Anchor href="https://google.com" target="_blank">
+            Anchor link
+          </Anchor>
+          <Button>Button</Button>
+          <DescriptionList>
+            <DescriptionListItem title="An entry with a title">
+              How good is that?
+            </DescriptionListItem>
+            <DescriptionListItem
+              titleElement={<strong>An entry with a title element</strong>}
+            >
+              How good is that?
+            </DescriptionListItem>
+          </DescriptionList>
+          <H2>H2 Heading</H2>
+          <H3>H3 Heading</H3>
+          <H4>H4 Heading</H4>
+          <H5>H5 Heading</H5>
+          <Input placeholder="Input element" />
+          <UnorderedList>
+            <li>One list item</li>
+            <li>Two list items</li>
+            <li>Three list items</li>
+            <li>Four list items</li>
+          </UnorderedList>
+        </Panel>
+      </Section>
+    </SingleColumnLayout>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+
+const root = ReactDOMClient.createRoot(container);
+
+root.render(<App />);
---
+++ example/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js,ts,jsx,tsx}', '../src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
